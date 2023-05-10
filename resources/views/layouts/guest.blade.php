bdd43cc35263034ec8a116e814329fc079115b54
Aurandi <aurandiemran1337@gmail.com>
2023-05-10 19:41:06 +0700

    Rubah warna pada page 'Profile'

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,7 +23,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-sky-300 shadow-xl overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-sky-200 shadow-xl overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
