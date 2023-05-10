7c1960cb1319a3389cb414828c6598faa268fb54
Aurandi <aurandiemran1337@gmail.com>
2023-05-10 12:57:02 +0700

    1. Routes 2. Dashboard dan About Us pagenya sudah dipisah 3. Welcome Page dirubah isinya 4. Edit2 UI

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,14 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <!-- About Us Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('aboutus')" :active="request()->routeIs('aboutus')">
+                        {{ __('About Us') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <!-- Settings Dropdown -->
