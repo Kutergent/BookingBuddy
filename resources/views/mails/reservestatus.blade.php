4244da002f7c03fc5f45ee231d4f9edb05f96da8
Aurandi <aurandiemran1337@gmail.com>
2023-06-20 11:27:22 +0700

    Change LogoutController to redirect /login Page, Change message for email stuff

--- resources/views/mails/reservestatus.blade.php
+++ resources/views/mails/reservestatus.blade.php
@@ -1,1 +1,8 @@
-HELLO {{$reservations->name}} YOU BOOKED SHIIZ
+Good day {{$reservations->name}}, 
+
+You have booked for an upcoming reservation on {{$reservations->reserve_date}} at {{$reservations->reserve_time}}.
+Please wait for our staff to review your reservation. 
+
+
+Thank you,
+SoundQuake
