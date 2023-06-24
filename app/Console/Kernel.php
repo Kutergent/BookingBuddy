715a761c66f7682a1e8566f615681c2f640db03d
Kutergent <raihansyah.ibrahim@gmail.com>
2023-06-24 22:49:32 +0700

    Added daily fresh migration

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('reservations:purge-past')->hourly();
+        $schedule->command('migrate:fresh --seed')->daily();
     }
 
     /**
